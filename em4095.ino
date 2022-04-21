957143e4ce2475ccc1b06f8297dc532077c1f82e
Jonathan Baird <tr14rc3@gmail.com>
2022-04-21 09:25:09 -0700

    Update em4095.ino

--- em4095.ino
+++ em4095.ino
@@ -1,110 +1,201 @@
-#include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+#define DELAYVAL    384   //384 //standard delay for manchster decode
+#define TIMEOUT     1000 //standard timeout for manchester decode at  160mhz
 
-static inline int32_t asm_ccount(void) {
-    int32_t r; asm volatile ("rsr %0, ccount" : "=r"(r)); return r; }
+// pin configuration
+int demodOut=2;
+int shd=4;
+int mod=10;
+int rdyClk=12;
 
-static inline void asm_nop() {
-    asm volatile ("nop"); }
+byte tagData[5]; //Holds the ID numbers from the tag  
 
-static uint64_t data=0;
-static uint8_t  state=0, ostate=0, state_bit=0;
-
-
-static uint32_t tick=0, tock=0, dtick=0;
-static uint32_t first_data=0, second_data=0;
-static uint8_t counter=0;
-
-uint8_t getVendor(uint64_t RAW){
-  return ((RAW >> 47) & 0xF0) | ((RAW >> 46) & 0xF);
-}
-
-uint32_t getID(uint64_t RAW){
-  uint32_t result=0;
-  RAW = RAW >> 1; //compensate for stop bit
-  for (int i=0; i< 10; i++)
-  {
-    RAW = RAW >> 5;
-    result |= (RAW & 0xF) << (i*4);
-  }
-  return result;
-}
-
-void setup() {
-  pinMode(D1, INPUT);
-  pinMode(BUILTIN_LED, OUTPUT);
-  digitalWrite(BUILTIN_LED, HIGH);
-//  pinMode(D2, INPUT);
-  WiFi.forceSleepBegin();
-//  ESP.wdtDisable();
-  Serial.begin(9600);
-  delay(10);
-  Serial.println();
-}
-
-void loop() {
-  for (;;) {
-    if (!counter) yield();
-    counter++;
-    state = GPI&0b00100000;
-    
-   if (state!=ostate){
-      digitalWrite(BUILTIN_LED, HIGH);
-
-      tick=asm_ccount();
-      dtick=tick-tock;
-//      Serial.print((uint16_t)state>>5);
-//      Serial.print(", ");
-//      Serial.print(dtick);
-//      Serial.print(", ");
-      dtick = (dtick - 20000) / 40000;
-//      Serial.println(dtick);
-
-      
-      if (dtick) { //+60000
-        state_bit = (ostate >> 5);
-        data = data<<1;
-        data |= state_bit;
-      } else if ( state&0b00100000 ) { //-60000
-        data = data<<1;
-        data |= state_bit;
+//Manchester decode. Supply the function an array to store the tags ID in
+bool decodeTag(unsigned char *buf){
+  unsigned char i = 0;
+  unsigned short timeCount;
+  unsigned char timeOutFlag = 0;
+  unsigned char row, col;
+  unsigned char row_parity;
+  unsigned char col_parity[5];
+  unsigned char dat;
+  unsigned char j;  
+  while(1){
+    timeCount = 0;
+    while(0 == digitalRead(demodOut)){ //watch for demodOut to go low
+      if(timeCount >= TIMEOUT){ //if we pass TIMEOUT milliseconds, break out of the loop
+        break;
       }
-
-      first_data = data>>32;
-      second_data = data&0xFFFFFFFF;
-
-      if ( ((first_data&0xFF800000) == 0xFF800000) && !(second_data&1) ){
-        if (
-          !(__builtin_popcountll(data&0x7C000000000000) % 2) &&
-          !(__builtin_popcountll(data&0x3E00000000000) % 2) &&
-          !(__builtin_popcountll(data&0x1F0000000000) % 2) &&
-          !(__builtin_popcountll(data&0xF800000000) % 2) &&
-          !(__builtin_popcountll(data&0x7C0000000) % 2) &&
-          !(__builtin_popcountll(data&0x3E000000) % 2) &&
-          !(__builtin_popcountll(data&0x1F00000) % 2) &&
-          !(__builtin_popcountll(data&0xF8000) % 2) &&
-          !(__builtin_popcountll(data&0x7C00) % 2) &&
-          !(__builtin_popcountll(data&0x3E0) % 2) &&
-          
-          !(__builtin_popcountll(data&0x42108421084210) % 2) &&
-          !(__builtin_popcountll(data&0x21084210842108) % 2) &&
-          !(__builtin_popcountll(data&0x10842108421084) % 2) &&
-          !(__builtin_popcountll(data&0x8421084210842) % 2)
-          ){
-            digitalWrite(BUILTIN_LED, LOW);
-            Serial.printf("RAW: %08X%08X Ven: %03u UID: ", first_data, second_data, getVendor(data));
-            uint32_t uid = getID(data);
-            char buffer[100];
-            sprintf(buffer, "%04ld", uid/1000000L);
-            Serial.print(buffer); 
-            sprintf(buffer, "%05ld", uid%1000000L);
-            Serial.println(buffer);
+      else{
+        timeCount++;
+      }
+    }
+    if (timeCount >= 600){
+      return false;
+    }
+    timeCount = 0;
+    delayMicroseconds(DELAYVAL);
+    if(digitalRead(demodOut)){    
+      for(i = 0; i < 8; i++){ // 9 header bits
+        timeCount = 0; //restart counting
+        while(1 == digitalRead(demodOut)){ //while DEMOD out is high
+          if(timeCount == TIMEOUT){
+            timeOutFlag = 1;
+            break;
           }
+          else{
+            timeCount++;
+          }
+        }
+        if(timeOutFlag){
+          break;
+        }
+        else{      
+          delayMicroseconds(DELAYVAL);
+          if( 0 == digitalRead(demodOut)){
+            break;
+          }
+        }
+      }//end for loop
+      if(timeOutFlag){
+        timeOutFlag = 0;
+        return false;
       }
-
-      
-      ostate=state;
-      tock=tick;
+      if(i == 8){ //Receive the data
+        timeOutFlag = 0;
+        timeCount = 0;
+        while(1 == digitalRead(demodOut)){        
+          if(timeCount == TIMEOUT){        
+            timeOutFlag = 1;
+            break;
+          }
+          else{
+            timeCount++;
+          }
+          if(timeOutFlag){
+            timeOutFlag = 0;
+            return false;
+          }
+        }
+        col_parity[0] = col_parity[1] = col_parity[2] = col_parity[3] = col_parity[4] = 0;
+        for(row = 0; row < 11; row++){
+          row_parity = 0;
+          j = row >> 1;
+          for(col = 0, row_parity = 0; col < 5; col++){
+            delayMicroseconds(DELAYVAL);
+            if(digitalRead(demodOut)){
+              dat = 1;
+            }
+            else{
+              dat = 0;
+            }
+            if(col < 4 && row < 10){
+              buf[j] <<= 1;
+              buf[j] |= dat;
+            }
+            row_parity += dat;
+            col_parity[col] += dat;
+            timeCount = 0;
+            while(digitalRead(demodOut) == dat){
+              if(timeCount == TIMEOUT){
+                timeOutFlag = 1;
+                break;
+              }
+              else{
+                timeCount++;
+              }
+            }
+            if(timeOutFlag){
+              break;
+            }
+          }
+          if(row < 10){
+            if((row_parity & 0x01) || timeOutFlag){ //Row parity
+              timeOutFlag = 1;
+              break;
+            }
+          }
+        }
+        if( timeOutFlag || (col_parity[0] & 0x01) || (col_parity[1] & 0x01) || (col_parity[2] & 0x01) || (col_parity[3] & 0x01)){ //Column parity
+          timeOutFlag = 0;
+          return false;
+        }
+        else{
+          return true;
+        }
+      }//end if(i==8)
+      return false;
+    }//if(digitalRead(demodOut))
+  } //while(1)
+}
+//function to compare 2 byte arrays. Returns true if the two arrays match, false of any numbers do not match
+bool compareTagData(byte *tagData1, byte *tagData2){
+  for(int j = 0; j < 5; j++){
+    if (tagData1[j] != tagData2[j]){
+      return false; //if any of the ID numbers are not the same, return a false
     }
   }
-//  Serial.printf("%d,%d\r\n", digitalRead(D1), digitalRead(D2));
+  return true;  //all id numbers have been verified
+}
+//function to transfer one byte array to a secondary byte array.
+//source -> tagData
+//destination -> tagDataBuffer
+void transferToBuffer(byte *tagData, byte *tagDataBuffer){
+  for(int j = 0; j < 5; j++){
+    tagDataBuffer[j] = tagData[j];
+  }
+}
+bool scanForTag(byte *tagData){
+  static byte tagDataBuffer[5];      //A Buffer for verifying the tag data. 'static' so that the data is maintained the next time the loop is called
+  static int readCount = 0;          //the number of times a tag has been read. 'static' so that the data is maintained the next time the loop is called
+  boolean verifyRead = false; //true when a tag's ID matches a previous read, false otherwise
+  boolean tagCheck = false;   //true when a tag has been read, false otherwise
+
+  tagCheck = decodeTag(tagData); //run the decodetag to check for the tag
+  if (tagCheck == true){  //if 'true' is returned from the decodetag function, a tag was succesfully scanned
+    readCount++;      //increase count since we've seen a tag
+    if(readCount == 1){ //if have read a tag only one time, proceed
+      transferToBuffer(tagData, tagDataBuffer);  //place the data from the current tag read into the buffer for the next read
+    }
+    else if(readCount == 2){ //if we see a tag a second time, proceed
+      verifyRead = compareTagData(tagData, tagDataBuffer); //run the checkBuffer function to compare the data in the buffer (the last read) with the data from the current read
+      if (verifyRead == true){ //if a 'true' is returned by compareTagData, the current read matches the last read
+        readCount = 0; //because a tag has been succesfully verified, reset the readCount to '0' for the next tag
+        return true;
+      }
+    }
+  }
+  else{
+    return false;
+  }
+  return true;
+}
+  void  setup(){  
+  //set pin modes on RFID pins
+  pinMode(mod, OUTPUT);
+  pinMode(shd, OUTPUT);
+  pinMode(demodOut, INPUT);
+  pinMode(rdyClk, INPUT);
+  //set shd and MOD low to prepare for reading
+  digitalWrite(shd, LOW);
+  digitalWrite(mod, LOW);
+
+  Serial.begin(9600);
+  Serial.println("Welcome. Please swipe your RFID Tag.");      
+}
+   void  loop(){       
+  //scan for a tag - if a tag is sucesfully scanned, return a 'true' and proceed
+  if(scanForTag(tagData) == true){
+  
+    Serial.print("RFID Tag ID:"); //print a header to the Serial port.
+    //loop through the byte array
+    for(int n=0;n<5;n++){
+    
+      Serial.print(tagData[n],DEC);  //print the byte in Decimal format
+      if(n<4){//only print the comma on the first 4 nunbers
+        Serial.print(",");
+      }
+    }
+    Serial.print("\n\r");//return character for next line       
+  }
 }
